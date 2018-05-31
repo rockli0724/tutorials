87807d883c82662cb3f3e214df76c8531cb26089
rockli0724 <Administrator@RockLi>
2018-05-31 21:40:57 +0800

    add method and constructor method

--- core-java-8/src/test/java/com/baeldung/java8/comparator/Employee.java
+++ core-java-8/src/test/java/com/baeldung/java8/comparator/Employee.java
@@ -14,9 +14,50 @@ public class Employee implements Comparable<Employee>{
     double salary;
     long mobile;
     
-    @Override
+    
+    protected Employee(String name, int age, double salary, long mobile) {
+		this.name = name;
+		this.age = age;
+		this.salary = salary;
+		this.mobile = mobile;
+	}
+
+	@Override
     public int compareTo(Employee argEmployee) {
         return name.compareTo(argEmployee.getName());
     }
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+
+	public long getMobile() {
+		return mobile;
+	}
+
+	public void setMobile(long mobile) {
+		this.mobile = mobile;
+	}
+    
     
 }
